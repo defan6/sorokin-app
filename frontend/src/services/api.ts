f8a6b8213462372dec8d7587bce7aeccbdce94e4
FedorVX <valayev2005@gmail.com>
2025-11-08 20:59:08 +0300

    k8s in process

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AuthService from './AuthService';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8080',
 });
 
 api.interceptors.request.use(
