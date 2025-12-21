52c3318a117c2ffc1e2df7442503bdc616e20998
codestroy333 <mr.kota200@gmail.com>
2025-12-21 16:19:00 +0700

    Add new service 'payment-service'

---
+++ payment-service/src/test/java/com/security/paymentserviceapplication/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.paymentserviceapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
